2a65d4f615ae5b61606a45a31763dfddcf5ec0b5
luanhaolun <347311090@qq.com>
2015-12-07 19:06:03 +0800

    第一次修改

--- src/com/example/gittestdemo/MainActivity.java
+++ src/com/example/gittestdemo/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class MainActivity extends Activity {
-
+	//第一次修改
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
